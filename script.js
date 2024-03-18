17ad4f661866d0c144a3753827369862cc5e3b1e
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-03-18 17:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // your code here
 document.getElementById('formSubmit').addEventListener('submit',function(e) {
 	e.preventDefault();
-	// const formData = new FormData(e.target)
-	// const name = formData.get('name')
-	// const year = formData.get('year')
-	const name = document.getElementById('name').value
-	const year = document.getElementById('year').value
-
+	const formData = new FormData(e.target)
+	const name = formData.get('name')
+	const year = formData.get('year')
+	// const name = document.getElementById('name').value
+	// const year = document.getElementById('year').value
+	if(name !== "" && year !== ""){
 	const formDataDisplay = document.getElementById('formDataDisplay');
-	formDataDisplay.innerHTML = `https://localhost:8080/?name=${name}&year=${year}`
+	formDataDisplay.innerHTML = `https://localhost:8080/?name=${name}&year=${year}`	
+	}
+	else if(name !== "" && year === ""){
+		const formDataDisplay = document.getElementById('formDataDisplay');
+	formDataDisplay.innerHTML = `https://localhost:8080/?name=${name}`
+	}
+	else if(name === "" && year !== ""){
+		const formDataDisplay = document.getElementById('formDataDisplay');
+	formDataDisplay.innerHTML = `https://localhost:8080/?year=${year}`
+	}
+	
 })
