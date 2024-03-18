af8f1d5949b2fd596107c8cb441937f68a9c720e
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-03-18 17:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+document.getElementById('formSubmit').addEventListener('submit',function(e) {
+	e.preventDefault();
+	// const formData = new FormData(e.target)
+	// const name = formData.get('name')
+	// const year = formData.get('year')
+	const name = document.getElementById('name').value
+	const year = document.getElementById('year').value
+
+	const formDataDisplay = document.getElementById('formDataDisplay');
+	formDataDisplay.innerHTML = `https://localhost:8080/?name=${name}&year=${year}`
+})
